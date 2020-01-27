a296e504fe7648679510d1684f9045f579819bab
DEL BARRIO PIZARRO Bladimir <b.delbarriopizarro@soprasteria.com>
2020-01-27 09:59:21 +0100

    feat:added login.component

--- src/modules/auth/components/login.component.js
+++ src/modules/auth/components/login.component.js
@@ -1,8 +1,33 @@
 import React from 'react';
 
 const Login = () => {
+    
+   const handleOnchange = () => {
+
+   }
+
     return(
-        <h1>Login</h1>
+        <div className="form-usuario">
+            <div className="contenedor-form sombra-dark">
+                <h1>Iniciar Sessión</h1>
+
+                <form>
+                    <div className="campo-form">
+                        <label htmlFor="email">Email</label>
+                         <input type="email" id="email" placeholder="Your email" onChange={handleOnchange}>
+                         </input>
+                    </div>
+                    <div className="campo-form">
+                         <label htmlFor="password">Password</label>
+                         <input type="password" id="password" placeholder="password" onChange={handleOnchange}>
+                         </input>
+                    </div>
+                    <div className="campo-form">
+                        <input type="submit" className="btn btn-primario btn-block" value="Iniciar Sessión"></input>
+                    </div>
+                </form>
+            </div>
+        </div>
     );
 }
 
